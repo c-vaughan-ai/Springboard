582456e3fd899e6cd9218212fc7e4be4e4759df6
conweezy <45926158+conweezy@users.noreply.github.com>
2019-03-25 12:27:00 -0700

    Add files via upload

---
+++ Capstone Proposal.R
@@ -0,0 +1,27 @@
+                                        #Capstone Proposal
+
+                                          #Connor Vaughan
+
+##The goal of this Capstone project is to examine the relationship between unemployment and variables 
+#such as race, gender, and age. (Over time) The research questions this project intends to answer is 
+#how has the differences in the unemployment rate between demographics changed over time as well as 
+#where are they expected to go in the future?
+
+##The results of this project will provide useful information to policymakers and employers. The 
+#unemployment rate is a key indicator used by policymakers to measure the strength of the economy 
+#and when unemployment falls below potential real GDP also falls. If policymakers can focus on specific 
+#groups of people who tend to be unemployed disproportionally, they can have a more significant impact on 
+#the economy when it enters a recession. Policymakers in the past have created some programs to support 
+#groups of people who tend to be unemployed at a higher rate than the general population, such as 
+#minorities, so it will be interesting to see how the relationship between race and unemployment has 
+#changed over time. Another insight this project may provide is to see if certain groups are affected by 
+#recessions (ex. 2007-2009) more than others. If so, policymakers can focus on these groups more to help 
+#get the economy out of a recession. 
+
+##To investigate this question, I will use data collected by the US Bureau of Labor statistics dating 
+#back to 1972. Organizing and cleaning this data will involve merging the various datasets and extracting 
+#the variables I intend to use, race, gender, age, and unemployment rate. After the data has been wrangled,
+#I will examine each variable individually as well as the relationship between different variables. 
+#Then I will create a forecasting model that aims to predict the future values of unemployment rates 
+#for different groups of interest. I can make these results available for policymakers so that they can 
+#better understand unemployment rates and its future projection.
